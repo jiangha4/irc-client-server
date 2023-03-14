08fa4925be8e598506d98e2ba4d8e3f60199c7e6
Haohan Jiang <haohan.jiang@nike.com>
2023-03-14 09:38:52 -0700

    working tox and pytest

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 import setuptools
 
 setuptools.setup(
-    version='0.1.0',
-    name='irc-server',
+    version="0.1.0",
+    name="irc-server",
     package_dir={"": "src"},
     test_suite="tests",
-    python_requires="3.7",
+    python_requires=">=3.7",
     description="IRC Server",
     url="https://github.com/jiangha4/irc-client-server",
-    author="davidjiang.haohan@gmail.com"
+    author="davidjiang.haohan@gmail.com",
+    extras_require={
+        "test": [
+            'pytest==7.1.3',
+            'pytest-html==3.1.1',
+            'pytest-cov',
+            'pylint',
+            'jinja2',
+            'black',
+        ],
+    },
 )
